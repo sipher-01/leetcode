3a541a431b887a6213d14e234ec7452670e29855
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-02-29 17:20:14 +0000

    Solve Word Ladder [cpp]

---
+++ word-ladder.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        	queue<pair<string,int>>q;
+	q.push({beginWord,1});
+	unordered_set<string>s(wordList.begin(),wordList.end());
+	s.erase(beginWord);
+	while(!q.empty()){
+		string word = q.front().first;
+		int step = q.front().second;
+		q.pop();
+		if(word == endWord)return step;
+		for(int i=0; i<word.length(); i++){
+			char original = word[i];
+			for(char ch='a'; ch<='z'; ch++){
+				word[i]= ch;
+				if(s.find(word)!=s.end()){
+					s.erase(word);
+					q.push({word,step+1});
+				}
+			}
+			word[i] = original;
+		}
+	}
+	return 0;
+    }
+};
