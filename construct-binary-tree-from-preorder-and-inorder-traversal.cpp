3924d6f832cdb81d9277cf61fcf8b7c45225c0ce
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-07-20 20:11:51 +0000

    Solve Construct Binary Tree from Preorder and Inorder Traversal [cpp]

--- construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -16,11 +16,10 @@ public:
     //     for(int i=0; i<inorder.size(); i++){
     //         mp[inorder[i]]=i;
     //     }
-    //     TreeNode* root = solve(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mp);
-    //     return root;
+    //     return solve(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mp);
     // }
     // TreeNode* solve(vector<int>& preorder, int preStart, int preEnd,  vector<int>& inorder, int inStart, int inEnd, unordered_map<int,int>mp){
-    //     if(preStart>preEnd || inStart>inEnd)return NULL;
+    //     if(preStart>preEnd || inStart>inEnd)return nullptr;
     //     TreeNode* root = new TreeNode(preorder[preStart]);
     //     int inroot = mp[root->val];
     //     int leftNodes = inroot-inStart;
@@ -28,7 +27,7 @@ public:
     //     root->right = solve(preorder,preStart+leftNodes+1,preEnd,inorder,inroot+1,inEnd,mp);
     // return root;
     // }
-        TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         unordered_map<int, int> mp;
         for (int i = 0; i < inorder.size(); i++) {
             mp[inorder[i]] = i;
@@ -37,15 +36,18 @@ public:
     }
 
     TreeNode* solve(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, unordered_map<int, int>& mp) {
-        if (preStart > preEnd || inStart > inEnd) return nullptr;
-        
-        TreeNode* root = new TreeNode(preorder[preStart]);
-        int inRoot = mp[root->val];
-        int leftNodes = inRoot - inStart;
+        if (preStart > preEnd || inStart > inEnd) {
+            return nullptr; // Base case: if the indices are out of bounds, return NULL
+        }
 
+        TreeNode* root = new TreeNode(preorder[preStart]); // Create the root node with the current root value from preorder
+        int inRoot = mp[root->val]; // Find the index of the root in the inorder sequence
+        int leftNodes = inRoot - inStart; // Calculate the number of nodes in the left subtree
+
+        // Recursively build the left and right subtrees
         root->left = solve(preorder, preStart + 1, preStart + leftNodes, inorder, inStart, inRoot - 1, mp);
         root->right = solve(preorder, preStart + leftNodes + 1, preEnd, inorder, inRoot + 1, inEnd, mp);
 
-        return root;
+        return root; // Return the constructed tree
     }
 };
