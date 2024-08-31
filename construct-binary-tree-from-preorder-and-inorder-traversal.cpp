f7b1ae169666eaba93bc04e4e1a8ba04db3b2ed1
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-08-31 23:39:17 +0000

    Solve Construct Binary Tree from Preorder and Inorder Traversal [cpp]

--- construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -13,19 +13,24 @@ class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         unordered_map<int,int>mp;
-        for(int i=0; i<inorder.size(); i++){
+        for(int i=0 ; i<inorder.size(); i++){
             mp[inorder[i]]=i;
         }
-        return solve(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mp);
-    }
-    TreeNode* solve(vector<int>& preorder, int preStart, int preEnd,  vector<int>& inorder, int inStart, int inEnd, unordered_map<int,int>&mp){
-        if(preStart>preEnd || inStart>inEnd)return nullptr;
-        TreeNode* root = new TreeNode(preorder[preStart]);
-        int inroot = mp[root->val];
-        int leftNodes = inroot-inStart;
-        root->left = solve(preorder,preStart+1,preStart+leftNodes,inorder,inStart,inroot-1,mp);
-        root->right = solve(preorder,preStart+leftNodes+1,preEnd,inorder,inroot+1,inEnd,mp);
-    return root;
+    return solve(preorder,0,preorder.size()-1,inorder,0,
+        inorder.size()-1,mp);
     }
 
+    TreeNode* solve(vector<int>& preorder,int prestart,int preend,
+     vector<int>& inorder,int instart, int inend,unordered_map<int,int>&mp){
+        if(prestart>preend || instart>inend)return nullptr;
+        int x = preorder[prestart];
+        TreeNode* root = new TreeNode(x);
+        int mid = mp[root->val];
+        int numLeft = mid-instart;
+        root->left = solve(preorder,prestart+1,prestart+numLeft,
+        inorder,instart,mid-1,mp);
+        root->right = solve(preorder,prestart+numLeft+1,preend,
+        inorder,mid+1,inend,mp);
+        return root;
+    }
 };
