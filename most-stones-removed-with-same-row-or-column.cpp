638b270b181a6cd213f81dcf0bae7d84d9910327
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-10 19:39:28 +0000

    Solve Most Stones Removed with Same Row or Column [cpp]

--- most-stones-removed-with-same-row-or-column.cpp
+++ most-stones-removed-with-same-row-or-column.cpp
@@ -1,3 +1,34 @@
+class Disjoint{
+public:
+    vector<int>rank,parent;
+    Disjoint(int v){
+        rank.resize(v+1,0);
+        parent.resize(v+1);
+        for(int i=0; i<=v; ++i){
+            parent[i] = i;
+        }
+    }
+    int findUparent(int x){
+        if(parent[x]==x)return x;
+        return parent[x] = findUparent(parent[x]);
+    }
+    
+    void unionByRank(int a,int b){
+        int pa = findUparent(a);
+        int pb = findUparent(b);
+        if(pa==pb)return ;
+        if(rank[pa]>rank[pb]){
+            parent[pb] = pa;
+        }
+        else if(rank[pa]<rank[pb]){
+            parent[pa] = pb;
+        }
+        else{
+            parent[pa] = pb;
+            rank[pb]++;
+        }
+    }
+};
 class Solution {
 public:
     void dfs(int ind, vector<vector<int>>& stones, vector<bool>& visi, int n){
@@ -11,11 +42,33 @@ public:
     }
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
-        vector<bool>visi(n);
-        int cnt = 0;
-        for(int i=0; i<n; i++){
-            if(!visi[i]){
-                dfs(i,stones,visi,n);
+        // vector<bool>visi(n);
+        // int cnt = 0;
+        // for(int i=0; i<n; i++){
+        //     if(!visi[i]){
+        //         dfs(i,stones,visi,n);
+        //         cnt++;
+        //     }
+        // }
+        // return n-cnt;
+        int maxrow = 0;
+        int maxcol = 0;
+        for(auto it : stones){
+            maxrow = max(maxrow,it[0]);
+            maxcol = max(maxcol,it[1]);
+        }
+        Disjoint ds(maxrow+maxcol+1);
+        unordered_map<int,int>mp;
+        for(auto it: stones){
+            int a = it[0];
+            int b = it[1]+maxrow+1;
+            ds.unionByRank(a,b);
+            mp[a]=1;
+            mp[b]=1;
+        }
+        int cnt=0;
+        for(auto it:mp){
+            if(ds.findUparent(it.first)==it.first){
                 cnt++;
             }
         }
