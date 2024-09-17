f2811a8cb7ac47b5ad3698de300b9d31fe860255
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-17 16:22:57 +0000

    Solve Assign Cookies [cpp]

---
+++ assign-cookies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int ans = 0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i = 0, j= 0;
+        while(i<g.size() && j<s.size()){
+            if(s[j]>=g[i]){
+                ans++;
+                j++;
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        return ans;
+    }
+};
