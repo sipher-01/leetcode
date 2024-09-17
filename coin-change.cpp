f53098b64d90419d584e2ca1fa989e6bc79c41eb
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-17 23:20:22 +0000

    Solve Coin Change [cpp]

--- coin-change.cpp
+++ coin-change.cpp
@@ -14,8 +14,24 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans = solve(n-1,amount,coins,dp);
-        if(ans == 1e9)return -1;
-        return ans;
+        // int ans = solve(n-1,amount,coins,dp);
+        // if(ans == 1e9)return -1;
+        // return ans;
+        for(int i=0; i<=amount; i++){
+            if(i%coins[0] == 0)dp[0][i] = i/coins[0];
+            else dp[0][i] = 1e9;
+        }
+        for(int i = 1; i<n ; i++){
+            for(int j = 0 ;j<=amount ; j++){
+                int np = 0 + dp[i-1][j];
+                int p = 1e9;
+                if(coins[i]<=j)p = 1 + dp[i][j-coins[i]] ;
+                dp[i][j] = min(p,np);
+            }
+        }
+
+        if(dp[n-1][amount]>=1e9)return -1;
+        return dp[n-1][amount];
+
     }
 };
