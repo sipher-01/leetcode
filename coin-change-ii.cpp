43dac317341f40591c3be221db11af9e1e37940e
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-18 23:51:29 +0000

    Solve Coin Change II [cpp]

--- coin-change-ii.cpp
+++ coin-change-ii.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
         int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        // vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        vector<int> prev(amount+1,0), curr(amount+1,0);
         for(int i=0; i<=amount; i++){
-            if(i%coins[0] == 0)dp[0][i] = 1;
-            else dp[0][i] = 0;
+            if(i%coins[0] == 0)prev[i] = 1;
+            else prev[i] = 0;
         }
         for(int i = 1; i<n ; i++){
             for(int j = 0 ;j<=amount ; j++){
-                int np =  dp[i-1][j];
+                int np =  prev[j];
                 int p = 0;
-                if(coins[i]<=j)p =  dp[i][j-coins[i]] ;
-                dp[i][j] = p+np;
+                if(coins[i]<=j)p =  curr[j-coins[i]] ;
+                curr[j] = p+np;
             }
+            prev = curr;
         }
 
-        // if(dp[n-1][amount]>=1e9)return -1;
-        return dp[n-1][amount];
+        return prev[amount];
 
     }
     int change(int amount, vector<int>& coins) {
