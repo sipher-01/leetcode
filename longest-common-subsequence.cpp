adedab0e842de14189cc46a313bf4e9b35d12cc8
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-20 18:54:17 +0000

    Solve Longest Common Subsequence [cpp]

---
+++ longest-common-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(int n, int m, string text1, string text2, vector<vector<int>>&dp){
+        if(n<0 || m<0)return 0;
+        if(dp[n][m]!=-1)return dp[n][m];
+        if(text1[n] == text2[m]) return dp[n][m] = 1 + solve(n-1,m-1,text1,text2,dp);
+        else return  dp[n][m] = max(solve(n-1,m,text1,text2,dp),
+        solve(n,m-1,text1,text2,dp));
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        // return solve(n-1,m-1,text1,text2,dp);
+        for(int i=0 ; i<=n; i++)dp[i][0]=0;
+        for(int i=0 ; i<=m; i++)dp[0][i]=0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(text1[i-1] == text2[j-1])dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = 0 + max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+    }
+};
