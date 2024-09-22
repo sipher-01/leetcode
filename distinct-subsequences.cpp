254765650880953ef81f6552ed139b7ce4effb64
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-22 22:20:14 +0000

    Solve Distinct Subsequences [cpp]

--- distinct-subsequences.cpp
+++ distinct-subsequences.cpp
@@ -12,16 +12,14 @@ int mod = 1e9+7;
         int n = s.size();
         int m = t.size();
         // vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        vector<int>prev(m+1,0),curr(m+1,0);
+        vector<int>dp(m+1,0);
         // return solve(n-1,m-1,s,t,dp);
-        prev[0]=curr[0]=1;
+        dp[0]=1;
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(s[i-1]==t[j-1])curr[j] = (prev[j-1] + prev[j])%mod;
-                else curr[j] = prev[j]%mod;
+            for(int j=m; j>=1; j--){
+                if(s[i-1]==t[j-1])dp[j] = (dp[j-1] + dp[j])%mod;
             }
-            prev = curr;
         }
-        return (int)prev[m];
+        return (int)dp[m];
     }
 };
