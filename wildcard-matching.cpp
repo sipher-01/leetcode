e03fc22c0c5ebbf0605823acc6a0ba1d1b7e377a
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-23 19:34:25 +0000

    Solve Wildcard Matching [cpp]

--- wildcard-matching.cpp
+++ wildcard-matching.cpp
@@ -22,27 +22,28 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<bool>>dp(n+1,vector<bool>(m+1,0));
+        // vector<vector<bool>>dp(n+1,vector<bool>(m+1,0));
         // return solve(n-1,m-1,s,p,dp);
-        dp[0][0] = true;
+        vector<bool>prev(m+1,false),curr(m+1,false);
+        prev[0] = true;
         for(int j=1; j<=m; j++){
             if(p[j-1]=='*'){
-                dp[0][j] = dp[0][j-1];
+                prev[j] = prev[j-1];
             }
         }
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
                 if(s[i-1]==p[j-1] || p[j-1]=='?') {
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }
-                else{
-                    if(p[j-1]=='*'){
-                        dp[i][j] = dp[i-1][j] | dp[i][j-1];
-                    }
+                else if(p[j-1]=='*'){
+                        curr[j] = prev[j] | curr[j-1];
                 }
+                else curr[j] = false;
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
 
     }
 };
