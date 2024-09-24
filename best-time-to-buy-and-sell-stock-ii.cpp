c87a5601d3dd4bc5bd9f45c4b0bd4f7364737eda
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-24 00:42:02 +0000

    Solve Best Time to Buy and Sell Stock II [cpp]

---
+++ best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(int i, int buy, vector<int>& prices,vector<vector<int>>&dp){
+        if(i==prices.size())return 0;
+        int profit = 0;
+        if(dp[i][buy]!=-1)return dp[i][buy];
+        if(buy){
+            profit = max(solve(i+1,0,prices,dp)-prices[i],
+            solve(i+1,1,prices,dp)-0);
+        }
+        else{
+            profit = max(prices[i]+solve(i+1,1,prices,dp),
+            0+solve(i+1,0,prices,dp));
+        }
+        return dp[i][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return solve(0,1,prices,dp);
+    }
+};
