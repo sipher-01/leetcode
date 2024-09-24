eb42fffe8f5db7f6f9a3071c940dd11424173dda
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-24 15:55:31 +0000

    Solve Best Time to Buy and Sell Stock with Transaction Fee [cpp]

--- best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
                 int n = prices.size();
-        vector<vector<int>> dp(n+2, vector<int>(2, 0)); 
+        // vector<vector<int>> dp(n+2, vector<int>(2, 0)); 
         // return solve(0,1,prices,dp);
+        vector<int>ahead(2,0),cur(2,0);
         for(int ind=n-1; ind>=0; ind--){
             for(int buy =0; buy<=1; buy++){
                 if (buy) {
-                    dp[ind][buy] = max(dp[ind + 1][0]- prices[ind],
-                    dp[ind + 1][1]);
+                    cur[buy] = max(ahead[0]- prices[ind],ahead[1]);
                 } else {
-                    dp[ind][buy] = max(dp[ind + 1][1] + prices[ind]-fee,
-                    dp[ind + 1][0]);
+                    cur[buy] = max(ahead[1] + prices[ind]-fee,ahead[0]);
                 }
             }
+            ahead = cur;
         }
-        return dp[0][1];
+        return ahead[1];
     }
 };
