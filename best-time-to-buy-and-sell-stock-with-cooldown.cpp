e9a6b3fbd868b4eb4ba02ea79fdd0056cf6237c0
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-24 15:51:20 +0000

    Solve Best Time to Buy and Sell Stock with Cooldown [cpp]

--- best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -14,7 +14,19 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1)); 
-        return solve(0,1,prices,dp);
+        vector<vector<int>> dp(n+2, vector<int>(2, 0)); 
+        // return solve(0,1,prices,dp);
+        for(int ind=n-1; ind>=0; ind--){
+            for(int buy =0; buy<=1; buy++){
+                if (buy) {
+                    dp[ind][buy] = max(dp[ind + 1][0]- prices[ind],
+                    dp[ind + 1][1]);
+                } else {
+                    dp[ind][buy] = max(dp[ind + 2][1] + prices[ind],
+                    dp[ind + 1][0]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
