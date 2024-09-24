7c44dd2e906107945722a5e79cc96cd49fb788f5
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-24 15:25:13 +0000

    Solve Best Time to Buy and Sell Stock III [cpp]

--- best-time-to-buy-and-sell-stock-iii.cpp
+++ best-time-to-buy-and-sell-stock-iii.cpp
@@ -20,7 +20,30 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,
-        vector<int>(3,-1)));
-        return solve(0,1,2,prices,dp);
+        vector<int>(3,0)));
+        // return solve(0,1,2,prices,dp);
+        // dp[n][0][0] = dp[n][0][1] = dp[n][0][2] = 0;
+        // dp[n][1][0] = dp[n][1][1] = dp[n][1][2] = 0;
+        // for(int i=0; i<=n; i++){
+        //     dp[i][0][0] = 0;
+        //     dp[i][1][0] = 0;
+        // }
+        for(int ind=n-1; ind>=0; ind--){
+            for(int buy=0; buy<2; buy++){
+                for(int cap=1; cap<=2; cap++){
+                    int profit = 0;
+                    if(buy){
+                        profit = max(dp[ind+1][0][cap]-prices[ind],
+                            dp[ind+1][1][cap]);
+                    }
+                    else{
+                        profit = max(dp[ind+1][1][cap-1]+prices[ind],
+                            dp[ind+1][0][cap]);
+                    }
+                    dp[ind][buy][cap] = profit;
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
