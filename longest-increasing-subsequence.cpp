d8154f1f0026d33920e1c90c14e3fed293f2ac69
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-24 17:03:10 +0000

    Solve Longest Increasing Subsequence [cpp]

--- longest-increasing-subsequence.cpp
+++ longest-increasing-subsequence.cpp
@@ -12,7 +12,18 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n= nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(0,-1,nums,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // return solve(0,-1,nums,dp);
+        for(int ind=n-1; ind>=0; ind--){
+            for(int prev=ind-1; prev>=-1; prev--){
+                    int p = 0;
+                    if(prev==-1 || nums[ind]>nums[prev]){
+                        p = 1 + dp[ind+1][ind+1];
+                    }
+                    int np = dp[ind+1][prev+1];
+                    dp[ind][prev+1] = max(p,np);
+            }
+        }
+        return dp[0][0];
     }
 };
