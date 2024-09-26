9e5c2dbe25a6135b46cdce4d38a5e4d470b4ae3b
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-26 18:59:52 +0000

    Solve Burst Balloons [cpp]

---
+++ burst-balloons.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(int i, int j, vector<int>& nums,vector<vector<int>>&dp){
+        if(i>j)return 0;
+        int maxi = -1e9;
+        if(dp[i][j]!=-1)return dp[i][j];
+        for(int ind = i; ind<=j; ind++){
+            int coins = nums[i-1]*nums[ind]*nums[j+1]+solve(i,ind-1,nums,dp)
+            + solve(ind+1,j,nums,dp);
+            maxi = max (maxi,coins);
+        }
+        return dp[i][j] = maxi;
+    }
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.push_back(1);
+        nums.insert(nums.begin(),1);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solve(1,n,nums,dp);
+    }
+};
