94e6fc29fd6be9882b094e3797cf625afb91fa4a
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-09-28 00:46:17 +0000

    Solve Integer to Roman [cpp]

--- integer-to-roman.cpp
+++ integer-to-roman.cpp
@@ -1,74 +1,12 @@
 class Solution {
 public:
     string intToRoman(int num) {
-    vector<int>n;
-    int j = 1;
-    while(num>0){
-        int i = num%10;
-        n.push_back(i*j);
-        j *= 10;
-        num /= 10;
-    }
-    reverse(n.begin(),n.end());
-    string ans="";
-    for(auto it: n){
-        if(it>=1000){
-            int times = it/1000;
-            while(times--){
-                ans += 'M';
-            }
-        }
-        else if(it>=500){
-            if(it==900)ans+="CM";
-            else{int times = (it-500)/100;
-            ans += 'D';
-            while(times--){
-                ans+='C';
-            }}
-        }
-        else if(it>=100){
-            if(it==400)ans+="CD";
-            else{
-            int times = (it)/100;
-            while(times--){
-                ans+='C';
-            }}
-        }
-        else if(it>=50){
-            if(it==90)ans+="XC";
-            else {int times = (it-50)/10;
-            ans +='L';
-            while(times--){
-                ans+='X';
-            }}
-        }
-        else if(it>=10){
-            if(it==40)ans+="XL";
-            else {
-            int times = (it)/10;
-            while(times--){
-                ans+='X';
-            }}
-        }
-        else if(it>=5){
-            if(it==9)ans+="IX";
-            else {
-            int times = (it-5);
-            ans+='V';
-            while(times--){
-                ans+='I';
-            }}
-        }
-        else{
-            if(it==0)continue;
-            if(it==4)ans+="IV";
-            else{
-                while(it--){
-                    ans+='I';
-                }
-            }
-        }
-    }
-        return ans;
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string ths[]={"","M","MM","MMM"};
+        
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + 
+        ones[num%10];
     }
 };
