41cbe58ed4ebff14041f7a4fc04c3cfec88b8948
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-10-15 19:55:32 +0000

    Solve Separate Black and White Balls [cpp]

---
+++ separate-black-and-white-balls.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long ans = 0;
+        int cnt = 0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='1')cnt+=1;
+            else  ans += cnt;
+        }
+        return ans;
+    }
+};
