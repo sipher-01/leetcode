b6bc65ef8248560a44e5551734445b4401517462
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-10-23 16:53:08 +0000

    Solve Cousins in Binary Tree II [cpp]

--- cousins-in-binary-tree-ii.cpp
+++ cousins-in-binary-tree-ii.cpp
@@ -12,43 +12,44 @@
 class Solution {
 public:
     TreeNode* replaceValueInTree(TreeNode* root) {
-        vector<int>levelSum;
+        // vector<int>levelSum;
         queue<TreeNode*>q;
         q.push(root);
+        // while(!q.empty()){
+        //     int n = q.size();
+        //     int sum = 0;
+        //     for(int i=0; i<n; i++){
+        //         TreeNode* node = q.front();
+        //         q.pop();
+        //         sum += node->val;
+        //         if(node->left)q.push(node->left);
+        //         if(node->right)q.push(node->right);
+        //     }
+        //     levelSum.push_back(sum);
+        // }
+        int levelSum = root->val;
         while(!q.empty()){
             int n = q.size();
-            int sum = 0;
-            for(int i=0; i<n; i++){
+            int nextLevel = 0;
+            while(n--){
                 TreeNode* node = q.front();
                 q.pop();
-                sum += node->val;
-                if(node->left)q.push(node->left);
-                if(node->right)q.push(node->right);
-            }
-            levelSum.push_back(sum);
-        }
-        q.push(root);
-        root->val = 0;
-        int level = 0;
-        while(!q.empty()){
-            int n = q.size();
-
-            level++ ;
-            for(int i=0; i<n; i++){
-                TreeNode* node = q.front();
-                q.pop();
+                node->val = levelSum - node->val;
                 int sibling = 0;
                 if(node->left)sibling += node->left->val;
                 if(node->right)sibling += node->right->val;
                 if(node->left){
-                    node->left->val = levelSum[level] - sibling;
+                    nextLevel += node->left->val;
+                    node->left->val = sibling;
                     q.push(node->left);
                 }
                 if(node->right){
-                    node->right->val = levelSum[level] - sibling;
+                    nextLevel += node->right->val;
+                    node->right->val = sibling;
                     q.push(node->right);
                 }
             }
+            levelSum = nextLevel;
 
         }
         return root;
