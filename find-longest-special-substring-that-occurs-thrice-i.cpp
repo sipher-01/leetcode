ad2ec27a3cae437ed61c129eb2c08235c020c7ff
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-12-10 18:36:55 +0000

    Solve Find Longest Special Substring That Occurs Thrice I [cpp]

--- find-longest-special-substring-that-occurs-thrice-i.cpp
+++ find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int maximumLength(string s) {
-        unordered_map<string,int>mp;
+        map<pair<char,int>,int>mp;
 
         for(int i=0; i<s.length(); i++){
-            string cur ="";
+            char cur =s[i];
             for(int j=i; j<s.length(); j++){
-                if(cur.empty() || cur.back() == s[j]){
-                    cur += s[j];
-                    mp[cur]++;
+                if(cur == s[j]){
+                    mp[{cur,j-i+1}]++;
                 }else{
                     break;
                 }
@@ -16,8 +15,8 @@ public:
         }
         int res = 0;
         for(auto it: mp){
-            if(it.second>=3 && it.first.length()>res){
-                res = it.first.length();
+            if(it.second>=3 && it.first.second>res){
+                res = it.first.second;
             }
         }
         return res==0?-1:res;
