5f195a8ed5ec7f5e7f1e3900860b498a5a32cb52
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-12-11 16:15:20 +0000

    Solve Maximum Beauty of an Array After Applying Operation [cpp]

---
+++ maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private: 
+    int bs(vector<int>& nums, int x){
+        int l=0,r=nums.size()-1;
+        int ans = 0;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(nums[mid]<=x){
+                ans = mid;
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
+        return ans;
+    }
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int res = 0;
+        for(int i=0; i<nums.size(); i++){
+            int end = bs(nums,nums[i]+ 2*k);
+            res = max(res,end-i+1);
+        }
+        return res;
+    }
+};
