24bafeafffe43b68ffcb45b2dff98ff49dcb5512
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-12-20 23:28:23 +0000

    Solve Reverse Odd Levels of Binary Tree [cpp]

---
+++ reverse-odd-levels-of-binary-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* l, TreeNode* r, int level){
+        if(!l || !r)return ;
+        if(level%2==1){
+            swap(l->val, r->val);
+        }
+        dfs(l->left,r->right,level+1);
+        dfs(l->right,r->left,level+1);
+    }
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        if (!root) return nullptr;
+        dfs(root->left,root->right,1);
+        return root;
+    }
+};
