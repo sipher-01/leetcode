e9600a0abb95617388d017f3ecafa0cbdd17805c
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-12-20 23:39:31 +0000

    Solve Symmetric Tree [cpp]

--- symmetric-tree.cpp
+++ symmetric-tree.cpp
@@ -11,14 +11,15 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* root,TreeNode* root1){
-        if(!root && !root1)return true;
-        if(!root || !root1)return false;
-        if(root->val!=root1->val)return false;
-        return solve(root->left,root1->right) && solve(root->right,root1->left);
-
+    bool dfs(TreeNode* l, TreeNode* r){
+        if(l==NULL && r==NULL)return true;
+        if(!l || !r)return false;
+        if(l->val != r->val)return false;
+        bool t = dfs(l->left,r->right);
+        bool t1 = dfs(l->right,r->left);
+        return t && t1;
     }
     bool isSymmetric(TreeNode* root) {
-      return solve(root,root);
+        return dfs(root->left,root->right);
     }
 };
