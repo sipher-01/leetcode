4fe3f435a91c383cd605b49a407cded4d609eb01
sipher-01 <sourav2003singhkatoch@gmail.com>
2024-12-27 13:27:13 +0000

    Solve Best Sightseeing Pair [cpp]

--- best-sightseeing-pair.cpp
+++ best-sightseeing-pair.cpp
@@ -3,15 +3,16 @@ public:
     int maxScoreSightseeingPair(vector<int>& values) {
         int n = values.size();
         int res = INT_MIN;
-        vector<int>temp(n);
-        for(int i=0; i<n; i++){
-            temp[i] = values[i] + i;
-        }
-        int l = temp[0];
+        // vector<int>temp(n);
+        // for(int i=0; i<n; i++){
+        //     temp[i] = values[i] + i;
+        // }
+        // int l = temp[0];
+        int l = values[0]+0;
         for(int i=1; i<n; i++){
             int r = values[i] - i;
             res = max(res,l+r);
-            l = max(l,temp[i]);
+            l = max(l,values[i]+i);
         }
 
         return res;
