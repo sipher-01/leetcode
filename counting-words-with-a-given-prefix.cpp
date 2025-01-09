e7fe1457ada9612f8d1707a710e41e7848b6d335
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-01-09 18:32:42 +0000

    Solve Counting Words With a Given Prefix [cpp]

--- counting-words-with-a-given-prefix.cpp
+++ counting-words-with-a-given-prefix.cpp
@@ -1,11 +1,64 @@
+class Node{
+public:
+    Node* arr[26];
+    int count;
+    Node(){
+        for(int i=0; i<26; i++){
+            arr[i] = nullptr;
+        }
+        count=0;
+    }
+};
+
+class Trie{
+    Node* root;
+public:
+    Trie(){
+        root = new Node();
+    }
+
+    void add(string& s){
+        Node* curr = root;
+        for(char c: s){
+            int ind = c-'a';
+            if(curr->arr[ind]==nullptr){
+                curr->arr[ind] = new Node();
+            }
+            curr = curr->arr[ind];
+            curr->count++;
+        }
+    }
+
+    int prefixCount(string& s){
+        Node* curr = root;
+        for(char c: s){
+            int ind = c-'a';
+            if (curr->arr[ind] == nullptr) {
+                return 0;
+            }
+            curr = curr->arr[ind];
+        }
+        return curr->count;
+    }
+};
+
+
+
+
+
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-        int l = pref.length();
-        int ans = 0;
-        for(auto it: words){
-            if(it.substr(0,l) == pref)ans++;
+        // int l = pref.length();
+        // int ans = 0;
+        // for(auto it: words){
+        //     if(it.substr(0,l) == pref)ans++;
+        // }
+        // return ans;
+        Trie trie;
+        for(auto word: words){
+            trie.add(word);
         }
-        return ans;
+        return trie.prefixCount(pref);
     }
 };
