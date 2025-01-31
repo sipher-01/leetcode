9ee4dc42ef3b887c1ad89977e15dbd6a37ca813f
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-01-31 17:59:40 +0000

    Solve Making A Large Island [cpp]

--- making-a-large-island.cpp
+++ making-a-large-island.cpp
@@ -35,6 +35,7 @@ public:
                 }
             }
         }
+        if(island==n*n)return island;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == 0){
