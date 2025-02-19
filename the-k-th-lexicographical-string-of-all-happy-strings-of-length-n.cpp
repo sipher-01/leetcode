61d86a431d588b1321d571be8ab81e82a90fe104
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-02-19 16:59:53 +0000

    Solve The k-th Lexicographical String of All Happy Strings of Length n [cpp]

--- the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -3,25 +3,31 @@ public:
     bool check(char a, char b){
         return a!=b;
     }
-    void solve(int i, int n, string temp,char prev, vector<string>&hs){
+    void solve(int i, int n, string temp,char prev,int &cnt, string&res, int k){
+        if(cnt>=k)return;
         if(i==n){
-            hs.push_back(temp);
+            cnt++;
+            if(cnt==k){
+                res = temp;
+            }
             return;
         }
         for(char c='a'; c<='c'; c++){
             if(check(c,prev)){
-                solve(i+1,n,temp+c,c,hs);
+                solve(i+1,n,temp+c,c,cnt,res,k);
             }
         }
         return;
     }
     string getHappyString(int n, int k) {
-        vector<string>happy_strings;
+        // vector<string>happy_strings;
+        int cnt = 0;
+        string res = "";
         for(char c='a'; c<='c'; c++){
             string temp(1,c);
-            solve(1,n,temp,c,happy_strings);
+            solve(1,n,temp,c,cnt,res,k);
+            if(!res.empty())return res;
         }
-        if(happy_strings.size()<k)return "";
-        return happy_strings[k-1];
+        return "";
     }
 };
