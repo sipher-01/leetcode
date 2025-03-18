5545b1f2f009d63b2aa1e0456e08210b92ef5264
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-03-18 16:01:45 +0000

    Solve Longest Nice Subarray [cpp]

---
+++ longest-nice-subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        
+        vector<int>bits(31,0);
+        int l=0;
+        int ans = 1;
+        int n = nums.size();
+        for(int r=0; r<n; r++){
+            for(int i=0; i<=30; i++){
+                if(nums[r] & (1<<i)){
+                    bits[i]++;
+                    while(bits[i]>1){
+                        for(int j=0; j<=30; j++){
+                            if(nums[l]&(1<<j))bits[j]--;
+                        }
+                        l++;
+                    }
+                }
+            }
+            ans = max(ans,r-l+1);
+        }
+        return ans;
+    }
+};
