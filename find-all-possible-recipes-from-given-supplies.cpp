da5278e4f06d9e0893c2fd80465830a08879a698
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-03-21 16:50:04 +0000

    Solve Find All Possible Recipes from Given Supplies [cpp]

--- find-all-possible-recipes-from-given-supplies.cpp
+++ find-all-possible-recipes-from-given-supplies.cpp
@@ -7,24 +7,49 @@ public:
             mp[it]++;
         }
         int n = recipes.size();
-        vector<bool>notMake(n,false);
-        int cnt = n;
-        while(cnt--){
-            for(int i=0; i<n; i++){
-                if(notMake[i])continue;
-                bool flag=true;
-                for(auto it: ingredients[i]){
-                    if(mp.find(it)==mp.end()){
-                        flag = false;
-                    }
-                }
-                if(flag){
-                    mp[recipes[i]]++;
-                    ans.push_back(recipes[i]);
-                    notMake[i] = true;
+        unordered_map<string,vector<int>>adj;
+        vector<int>indegree(n,0);
+        for(int i=0; i<n; i++){
+            for(auto it: ingredients[i]){
+                if(mp.find(it)==mp.end()){
+                    adj[it].push_back(i);
+                    indegree[i]++;
                 }
             }
         }
+        queue<int>q;
+        for(int i=0; i<n; i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int ind = q.front();
+            q.pop();
+            ans.push_back(recipes[ind]);
+            for(auto it: adj[recipes[ind]]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
+            }
+        }
+        // vector<bool>notMake(n,false);
+        // int cnt = n;
+        // while(cnt--){
+        //     for(int i=0; i<n; i++){
+        //         if(notMake[i])continue;
+        //         bool flag=true;
+        //         for(auto it: ingredients[i]){
+        //             if(mp.find(it)==mp.end()){
+        //                 flag = false;
+        //             }
+        //         }
+        //         if(flag){
+        //             mp[recipes[i]]++;
+        //             ans.push_back(recipes[i]);
+        //             notMake[i] = true;
+        //         }
+        //     }
+        // }
 
 
         return ans;
