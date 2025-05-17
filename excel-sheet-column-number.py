e7122d79c192f216b912e92e635b49c4a31dedf5
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-05-17 22:57:08 +0000

    Solve Excel Sheet Column Number [python3]

---
+++ excel-sheet-column-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        ans = 0
+        for ch in columnTitle:
+            ans = ans*26 + (ord(ch) - 64)
+        return ans
+        
+
+
+        
