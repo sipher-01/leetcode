018af697110ed211e9ba79946e22a9fc96f9c1d5
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-05-17 17:46:55 +0000

    Solve Path Sum [python3]

---
+++ path-sum.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # def solve(self,root,targetSum):
+    #     if root is None:
+    #         return False
+        
+    #     left = solve(root.left,target)
+
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return False
+        if targetSum == root.val and (not root.left and not root.right):
+            return True
+        taken = self.hasPathSum(root.left,targetSum - root.val) or self.hasPathSum(root.right,targetSum - root.val)
+
+        return taken 
+        
