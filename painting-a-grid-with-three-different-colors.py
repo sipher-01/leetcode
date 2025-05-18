1a62cd4f333bf46a292068c0a406a23014ab908f
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-05-18 17:22:30 +0000

    Solve Painting a Grid With Three Different Colors [python3]

---
+++ painting-a-grid-with-three-different-colors.py
@@ -0,0 +1,58 @@
+class Solution:
+
+    def __init__(self):
+        self.colour_string = []
+        self.mod = 10**9 + 7
+        self.memo = []
+
+    def generateColumn(self,cur_string,prev_char,l,m):
+        if l==m:
+            self.colour_string.append(cur_string)
+            return
+        
+        for ch in ('R','G','B'):
+            if prev_char == ch:
+                continue
+            self.generateColumn(cur_string+ch,ch,l+1,m)
+
+
+    def solve(self,prev_idx,rem_column, m):
+        if rem_column==0:
+            return 1
+        if self.memo[rem_column][prev_idx]!=-1:
+            return self.memo[rem_column][prev_idx]
+        prev_column = self.colour_string[prev_idx]
+        ways = 0
+        for i in range(len(self.colour_string)):
+            if i == prev_idx:
+                continue
+            cur_column = self.colour_string[i]
+            valid = True
+            for j in range(m):
+                if cur_column[j]==prev_column[j]:
+                    valid = False
+                    break
+            if valid:
+                ways = (ways + self.solve(i,rem_column-1,m))%self.mod
+        
+        self.memo[rem_column][prev_idx] = ways
+        return ways
+
+    def colorTheGrid(self, m: int, n: int) -> int:
+        #step 1 : generate all column strings
+        self.generateColumn("",'#',0,m)
+
+        
+        #step 2: find all ways to fill columns with above found column strings
+        res = 0
+        k = len(self.colour_string)
+        self.memo = [[-1]*(k+1) for _ in range(n+1)]
+
+        for i in range(k):
+
+            res = (res + self.solve(i,n-1,m))%self.mod
+        
+        return res
+
+
+        
