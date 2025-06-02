c823124218c9cef89e237c515afb2fb6b948b091
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-06-02 23:47:58 +0000

    Solve Candy [python3]

--- candy.py
+++ candy.py
@@ -1,11 +1,13 @@
-class Solution:
-    def candy(self, ratings: List[int]) -> int:
-        arr = [1]*len(ratings);
-        for i in range(1, len(ratings)):
-            if(ratings[i-1]<ratings[i]):
-                arr[i] = arr[i-1]+1
-        for i in range(len(ratings)-2,-1,-1):
-            if(ratings[i]>ratings[i+1]):
-                arr[i] = max(arr[i],arr[i+1]+1)
-
-        return sum(arr)
+class Solution(object):
+    def candy(self, ratings):
+        n = len(ratings)
+        cnt = 0
+        candies = [1] * n
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        for i in range(n - 1, 0, -1):
+            if ratings[i - 1] > ratings[i]:
+                candies[i - 1] = max(candies[i] + 1, candies[i - 1])
+            cnt += candies[i - 1]
+        return cnt + candies[n - 1]
