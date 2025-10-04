fa8c5582ab0ccdc4eadb88f2ee18904062fdfd07
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-10-04 12:32:25 +0000

    Solve Container With Most Water [cpp]

--- container-with-most-water.cpp
+++ container-with-most-water.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n = height.size();
-        int i = 0;
-        int j = n-1;
-        int ans= 0;
+        int i=0, j = height.size()-1;
+        int ans = 0;
         while(i<j){
+            int h = min(height[i],height[j]);
             int w = j-i;
-            int h;
-            if(height[i]>height[j]){
-                h=height[j];
-                j--;
-            }
-            else{
-                h = height[i];
-                i++;
-            }
-                ans = max(ans,w*h);
-            
+            int water = h*w;
+            ans = max(ans,water);
+            if(height[i]<height[j])i++;
+            else j--;
         }
-        return ans;
 
+        return ans;
     }
 };
