3f3283a96e888341b05b3598942f33a0d5ef91a7
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-11-09 17:53:58 +0000

    Solve Count Operations to Obtain Zero [cpp]

---
+++ count-operations-to-obtain-zero.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int ans = 0;
+        
+        while(true){
+            if(num1>=num2){
+                if(num1==0 || num2==0)break;
+                ans+= num1/num2;
+                num1 = num1%num2;
+            }
+            else{
+                if(num1==0 || num2==0)break;
+                ans += num2/num1;
+                num2 = num2%num1;
+            }
+        }
+        return ans;
+    }
+};
