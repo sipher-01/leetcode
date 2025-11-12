6222bdd0ea8216c745d37061e4f3b53399e7fdbe
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-11-12 12:01:26 +0000

    Solve Minimum Number of Operations to Make All Array Elements Equal to 1 [cpp]

---
+++ minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int gcd(int a,int b){
+        if(b>a){
+            return gcd(b,a);
+        }
+        if(b==0)return a;
+        return gcd(b,a%b);
+    }
+    int minOperations(vector<int>& nums) {
+       int n = nums.size();
+        int num1 = 0;
+        int g = 0;
+        for (int x : nums) {
+            if (x == 1) {
+                num1++;
+            }
+            g = gcd(g, x);
+        }
+        if (num1 > 0) {
+            return n - num1;
+        }
+        if (g > 1) {
+            return -1;
+        }
+        int min_len = n;
+        for (int i = 0; i < n; i++) {
+            int g = 0;
+            for (int j = i; j < n; j++) {
+                g = gcd(g, nums[j]);
+                if (g == 1) {
+                    min_len = min(min_len, j - i + 1);
+                    break;
+                }
+            }
+        }
+        return min_len + n - 2;
+    }
+};
