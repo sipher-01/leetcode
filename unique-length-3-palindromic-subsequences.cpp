043d62b431d6fb0493f13137831dac2cba94c357
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-11-21 12:44:58 +0000

    Solve Unique Length-3 Palindromic Subsequences [cpp]

--- unique-length-3-palindromic-subsequences.cpp
+++ unique-length-3-palindromic-subsequences.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
+    bool check(string& t){
+        if(t[0]==t[2])return 1;
+        return 0;
+    }
+    void solve(int i, string& s , string t, set<string>&st){
+        if(i>s.length())return;
+        if(t.length()==3 && check(t)){
+            st.insert(t);
+            return;
+        }
+
+        solve(i+1,s,t,st);
+        solve(i+1,s,t+s[i],st);
+    } 
     int countPalindromicSubsequence(string s) {
-                vector<int> R(26,0);
-        for(auto &u : s)
-        {
-            R[u-'a']++;
-        }
-        vector<int> L(26, 0);
-        
-        unordered_set<int> S;
-        
-        for(int i = 0; i < s.length(); i++)
-        {
-            int t = s[i] - 'a';
-            R[t]--;
-            for(int j = 0; j < 26; j++)
-            {
-                if(L[j] > 0 && R[j] > 0)
-                {
-                    S.insert(26*t + j);
-                }
+        // set<string>st;
+        // solve(0,s,"",st);
+        // return st.size();
+        int n = s.length();
+        vector<int>first(26,-1);
+        vector<int>last(26,-1);
+        for(int i=0; i<n; i++){
+            int val = s[i] - 'a';
+            if(first[val]==-1){
+                first[val] = i;
             }
-            L[t]++;
+            last[val] = i;
         }
-        
-        return S.size();
+        int ans = 0;
+        for(int i=0; i<26; i++){
+            unordered_set<char>st;
+            for(int j=first[i]+1; j<last[i]; j++){
+                st.insert(s[j]);
+            }
+            ans+=st.size();
+        }
+        return ans;
     }
 };
