a5e8f026e3da77d4b4f38e678ef6c2da4b026adb
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-12-08 19:57:39 +0000

    Solve Count Square Sum Triples [cpp]

--- count-square-sum-triples.cpp
+++ count-square-sum-triples.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int countTriples(int n) {
-        unordered_set<int>sq;
-        for(int i=1; i<=n; i++){
-            sq.insert(i*i);
-        }
-        int ans = 0;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                int sum = i*i + j*j;
-                if(sq.find(sum)!=sq.end()){
-                    ans++;
+        int res = 0;
+        // enumerate a and b
+        for (int a = 1; a <= n; ++a) {
+            for (int b = 1; b <= n; ++b) {
+                // determine if it meets the requirements
+                int c = int(sqrt(a * a + b * b + 1.0));
+                if (c <= n && c * c == a * a + b * b) {
+                    ++res;
                 }
             }
         }
-        return ans;
+        return res;
     }
 };
