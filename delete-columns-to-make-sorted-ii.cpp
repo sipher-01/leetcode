37494bece44690d2bd69c9fbdac478739d3f5369
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-12-21 19:05:59 +0000

    Solve Delete Columns to Make Sorted II [cpp]

--- delete-columns-to-make-sorted-ii.cpp
+++ delete-columns-to-make-sorted-ii.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int n=strs.size(), m=strs[0].size();
-        bitset<100> isSorted=0, remove=0;
-        for (int j=0; j<m; j++) {
-            for (int i=0; i<n-1; i++) {
-                if (isSorted[i]==0 & strs[i][j]>strs[i+1][j]) {
-                    remove[j]=1;
+        int n = strs.size();
+        int m = strs[0].size();
+        int unresolved = n-1;
+        vector<int>resolved(n,0);
+        int res = 0;
+        for(int j=0; j<m && unresolved>0; j++){
+            bool flag = false;
+            for(int i=1; i<n; i++){
+                if(!resolved[i] && strs[i][j]<strs[i-1][j]){
+                    flag = true;
                     break;
                 }
             }
-
-            if (remove[j]) continue;
-            
-            for (int i=0; i<n-1; i++) 
-                isSorted[i]=isSorted[i]|(strs[i][j]<strs[i+1][j]);
+            if(flag){
+                res++;
+                continue;
+            }
+            for(int i=1; i<n ; i++){
+                if(!resolved[i] && strs[i][j]>strs[i-1][j]){
+                    resolved[i] = 1;
+                    unresolved--;
+                }
+            }
         }
-        return remove.count();
+        return res;
     }
 };
