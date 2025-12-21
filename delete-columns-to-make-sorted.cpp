bfab8ed314226f7f887634d4d575b32634a75e50
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-12-21 12:31:33 +0000

    Solve Delete Columns to Make Sorted [cpp]

--- delete-columns-to-make-sorted.cpp
+++ delete-columns-to-make-sorted.cpp
@@ -10,7 +10,13 @@ public:
     int minDeletionSize(vector<string>& strs) {
         int res = 0;
         for (int j = 0; j < strs[0].size(); j++)
-            res += isUnsorted(strs, j);
+            // res += isUnsorted(strs, j);
+            for(int i=1; i<strs.size(); i++){
+                if(strs[i][j]<strs[i-1][j]){
+                    res++;
+                    break;
+                    }
+            }
         return res;
     }
 };
