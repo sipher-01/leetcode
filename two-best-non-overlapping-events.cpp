6682bd9838cb330a337271bee1090d0dbd5d14e7
sipher-01 <sourav2003singhkatoch@gmail.com>
2025-12-24 01:54:01 +0000

    Solve Two Best Non-Overlapping Events [cpp]

--- two-best-non-overlapping-events.cpp
+++ two-best-non-overlapping-events.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    int solve(int i, vector<vector<int>>& events,int count,vector<vector<int>>&dp){
-        if(count==2 || i>=events.size())return 0;
-        if(dp[i][count]!=-1)return dp[i][count];
-        int nextInd = bs(events,events[i][1]);
-        int take = events[i][2] + solve(nextInd,events,count+1,dp);
-        int notTake = solve(i+1,events,count,dp);
-
-        return dp[i][count]=max(notTake,take); 
-
-    }
-    int bs(vector<vector<int>>& events,int x){
-        int l =0;
-        int n = events.size();
-        int r = n-1;
-        int res;
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(events[mid][0]<=x){
-                l = mid+1;
-            }
-            else{
-                res = mid;
-                r = mid-1;
-            }
-        }
-
-        return res;
-    }
     int maxTwoEvents(vector<vector<int>>& events) {
-        sort(events.begin(),events.end());
-        vector<vector<int>>dp(100001,vector<int>(3,-1));
-        return solve(0,events,0,dp);
-
+        sort(events.begin(), events.end());
+        vector<vector<int>> dp(events.size(), vector<int>(3, -1));
+        return findEvents(events, 0, 0, dp);
+    }
+    // Recursive function to find the greatest sum for the pairs.
+    int findEvents(vector<vector<int>>& events, int idx, int cnt,
+                   vector<vector<int>>& dp) {
+        if (cnt == 2 || idx >= events.size()) return 0;
+        if (dp[idx][cnt] == -1) {
+            int end = events[idx][1];
+            int lo = idx + 1, hi = events.size() - 1;
+            while (lo < hi) {
+                int mid = lo + ((hi - lo) >> 1);
+                if (events[mid][0] > end)
+                    hi = mid;
+                else
+                    lo = mid + 1;
+            }
+            int include =
+                events[idx][2] + (lo < events.size() && events[lo][0] > end
+                                      ? findEvents(events, lo, cnt + 1, dp)
+                                      : 0);
+            int exclude = findEvents(events, idx + 1, cnt, dp);
+            dp[idx][cnt] = max(include, exclude);
+        }
+        return dp[idx][cnt];
     }
 };
