dd9e9ee18046e1d5eec7c771773a42ea09499559
sipher-01 <sourav2003singhkatoch@gmail.com>
2026-01-04 19:47:06 +0000

    Solve Number of Ways to Paint N × 3 Grid [cpp]

--- number-of-ways-to-paint-n-3-grid.cpp
+++ number-of-ways-to-paint-n-3-grid.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
-    int numOfWays(int n) {
-        const int MOD = 1000000007;
-        long long x = 6, y = 6;
-
-        for (int i = 2; i <= n; i++) {
-            long long new_x = (3 * x + 2 * y) % MOD;
-            long long new_y = (2 * x + 2 * y) % MOD;
-            x = new_x;
-            y = new_y;
+    const int mod = 1e9+7;
+    vector<string>base = {"RYR","RYG","RGR","RGY","YRY","YRG","YGR","YGY","GRY","GRG","GYR","GYG"};
+    vector<vector<int>>dp;
+    int solve(int n, int ind){
+        if(n==0){
+            return 1;
         }
-
-        return (x + y) % MOD;
+        if(dp[n][ind]!=-1)return dp[n][ind];
+        int res = 0;
+        for(int i=0; i<12; i++){
+            if(ind==i)continue;
+            bool flag = false;
+            for(int j=0; j<3; j++){
+                if(base[ind][j]==base[i][j]){
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                res = (res+solve(n-1,i))%mod;
+            }
+        }
+        return dp[n][ind] = res%mod;
+    }
+    int numOfWays(int n) {
+        int ans = 0;
+        dp.resize(n+1,vector<int>(13,-1));
+        for(int i=0; i<12; i++){
+            ans = (ans + solve(n-1,i))%mod;
+        }
+        return ans;
     }
 };
