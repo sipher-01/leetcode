82ed8b95c8bd61184e4b527d8b1ca9bf11311067
sipher-01 <sourav2003singhkatoch@gmail.com>
2026-01-05 21:00:51 +0000

    Solve Maximum Matrix Sum [cpp]

--- maximum-matrix-sum.cpp
+++ maximum-matrix-sum.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
+    using ll = long long;
     long long maxMatrixSum(vector<vector<int>>& matrix) {
+        ll ans = 0;
+        ll neg = INT_MAX;
         int n = matrix.size();
         int m = matrix[0].size();
-        int neg = 0;
-        long long total = 0;
-        int min_ele = INT_MAX;
+        int cnt = 0;
+        int zero = false;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(matrix[i][j]<0)neg++;
-                total += abs(matrix[i][j]);
-                min_ele = min(min_ele,abs(matrix[i][j]));
+                if(matrix[i][j]<0){
+                    cnt++;
+                }
+                else if(matrix[i][j]==0){
+                    zero = true;
+                }
+                ll num = abs(matrix[i][j]);
+                neg = min(neg,num);
+                ans += num;
             }
         }
-        if(neg%2==0)return total;
-        else{
-            return (total - (min_ele*2));
+        if(cnt%2 && !zero){
+            ans -= (2*neg);
         }
+        return ans;
     }
 };
