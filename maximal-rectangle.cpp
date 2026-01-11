f49855a78a1c74d0cb66dc6426ba792041be92f2
sipher-01 <sourav2003singhkatoch@gmail.com>
2026-01-11 19:27:42 +0000

    Solve Maximal Rectangle [cpp]

--- maximal-rectangle.cpp
+++ maximal-rectangle.cpp
@@ -1,43 +1,59 @@
 class Solution {
 public:
-    int longestHistogram(vector<int>&hist){
+
+    int longestHistogram(vector<int>&a, int n){
         stack<int>st;
-        int maxA = 0;
-        int n = hist.size();
-        for(int i=0; i<=n; i++){
-            while(!st.empty() && (i==n || hist[st.top()]>hist[i])){
-                int height = hist[st.top()];
+        vector<int>prev(n),next(n);
+        for(int i=0; i<n; i++){
+            while(!st.empty() && a[st.top()]>=a[i]){
                 st.pop();
-                int width;
-                if(st.empty()){
-                    width = i;
-                }
-                else{
-                    width = i-1 - st.top();
-                }
-                maxA = max(maxA,height*width);
+            }
+            if(st.empty())prev[i]=-1;
+            else{
+                prev[i] = st.top();
             }
             st.push(i);
         }
-        return maxA;
+        st = stack<int>();
+
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && a[st.top()]>=a[i]){
+                st.pop();
+            }
+            if(st.empty())next[i]=n;
+            else{
+                next[i] = st.top();
+            }
+            st.push(i);
+        }
+        int res=0;
+        for(int i=0; i<n; i++){
+            int length = next[i] - prev[i] - 1;
+            int height = a[i];
+            int area = length*height;
+            res = max(res,area);
+        }
+        return res;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int>hist(m,0);
+
+        vector<int>histogram(m,0);
         int ans = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(matrix[i][j]=='1'){
-                    hist[j]++;
+                    histogram[j]++;
                 }
                 else{
-                    hist[j] = 0;
+                    histogram[j] = 0;
                 }
             }
-            int maxA = longestHistogram(hist);
-            ans = max(maxA,ans);
+            int rect = longestHistogram(histogram,m);
+            ans = max(ans,rect);
         }
+
         return ans;
     }
 };
