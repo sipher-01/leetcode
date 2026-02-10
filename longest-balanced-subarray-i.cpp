62aee91fd4d6842f4ed6b34786c99b5a5345798b
sipher-01 <sourav2003singhkatoch@gmail.com>
2026-02-10 20:00:58 +0000

    Solve Longest Balanced Subarray I [cpp]

---
+++ longest-balanced-subarray-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            set<int>even,odd;
+            for(int j=i; j<n; j++){
+                if(nums[j]%2){
+                    even.insert(nums[j]);
+                }
+                else{
+                    odd.insert(nums[j]);
+                }
+                if(odd.size()==even.size()){
+                    ans = max(ans,j-i+1);
+                }
+            }
+        }
+        return ans;
+    }
+};
