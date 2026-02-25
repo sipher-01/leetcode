497415f28de6372b395a2dfd0906507bf647c219
sipher-01 <sourav2003singhkatoch@gmail.com>
2026-02-25 12:41:57 +0000

    Solve Sort Integers by The Number of 1 Bits [cpp]

---
+++ sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        map<int,vector<int>>mp;
+        for(int i=0; i<arr.size(); i++){
+            int cnt = __builtin_popcount(arr[i]);
+            mp[cnt].push_back(arr[i]);
+
+        }
+        vector<int>ans;
+        for(auto it:mp){
+            sort(it.second.begin(),it.second.end());
+            ans.insert(ans.end(),it.second.begin(),it.second.end());
+        }
+        return ans;
+    }
+};
