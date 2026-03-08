03552e3978e147bac14387bd11328033f0e7a25c
sipher-01 <sourav2003singhkatoch@gmail.com>
2026-03-08 15:50:04 +0000

    Solve Find Unique Binary String [cpp]

--- find-unique-binary-string.cpp
+++ find-unique-binary-string.cpp
@@ -1,15 +1,24 @@
 class Solution {
-
 public:
+    bool solve(int i, int n, set<string>&st, string temp, string &s){
+        if(i>=n){
+            if(st.count(temp)==0){
+                s = temp;
+                return true;
+            }
+            return false;
+        }
+
+        if (solve(i+1,n,st,temp+'0',s))return true;
+        if (solve(i+1,n,st,temp+'1',s))return true;
+        return false;
+    }
     string findDifferentBinaryString(vector<string>& nums) {
-            string result;
-                    for (int i = 0; i < nums.size(); i++) {
-                                if (nums[i][i] == '0') {
-                                                result += '1';
-                                                            } else {
-                                                                            result += '0';
-                                                                                        }
-                                                                                                }
-                                                                                                        return result;
-                                                                                                            }
-                                                                                                            };
+        set<string>st(nums.begin(),nums.end());
+        int n = nums[0].length();
+        if(st.size() == pow(2,n))return "";
+        string s = "";
+        solve(0,n,st,"",s);
+        return s;
+    }
+};
