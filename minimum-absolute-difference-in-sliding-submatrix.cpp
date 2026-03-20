3f6409aec6d4a1d6f0e1a425357cea4a02fc19b6
sipher-01 <sourav2003singhkatoch@gmail.com>
2026-03-20 12:55:33 +0000

    Solve Minimum Absolute Difference in Sliding Submatrix [cpp]

---
+++ minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+
+public:
+    vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        vector<vector<int>>ans;
+        int n = grid.size();
+        int m = grid[0].size();
+
+        for(int i=0; i<n; i++){
+            vector<int>temp;
+            for(int j=0; j<m; j++){
+                int nn = i + k;
+                int nm = j + k;
+                cout<<nn<<" "<<nm<<endl;
+                if(nn<=n && nm<=m){
+                    set<int>st;
+                    for(int k = i; k<nn; k++){
+                        for(int l = j; l<nm; l++){
+                            st.insert(grid[k][l]);
+                        }
+                    }
+                    int mini = INT_MAX;
+                    for(auto it = next(st.begin()); it!=st.end(); it++){
+                        int first = *it;
+                        int second = *prev(it);
+                        mini = min(mini, abs(second - first));
+                    }
+                    cout<<"mini : "<<mini<<endl;
+                    if(mini == INT_MAX) mini = 0;
+                    temp.push_back(mini);
+                }
+            }
+            if(temp.size()>0)ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
