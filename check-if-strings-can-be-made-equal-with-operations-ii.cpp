d9754290bd748b280eba333aaca3f32bf468168a
sipher-01 <sourav2003singhkatoch@gmail.com>
2026-03-30 13:08:20 +0000

    Solve Check if Strings Can be Made Equal With Operations II [cpp]

---
+++ check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        unordered_map<char,int>t1,t2;
+
+        for(int i=0; i<s1.length(); i++){
+            if(i%2==0){
+                t1[s1[i]]++;
+            }
+            else{
+                t2[s1[i]]++;
+            }
+        }
+
+        for(int i=0; i<s1.length(); i++){
+            if(i%2==0){
+                if(t1.count(s2[i])==0)return false;
+                else{
+                    t1[s2[i]]--;
+                }
+            }
+            else{
+                if(t2.count(s2[i])==0)return false;
+                else{
+                    t2[s2[i]]--;
+                }
+            }
+        }
+
+        for(auto it: t1){
+            if(it.second>0)return false;
+        }
+        for(auto it: t2){
+            if(it.second>0)return false;
+        }
+
+        return true;
+    }
+};
