42df7aff5bafc06c3573264862af1153c971bc13
sipher-01 <sourav2003singhkatoch@gmail.com>
2026-06-16 23:44:46 +0000

    Solve Process String with Special Operations I [cpp]

---
+++ process-string-with-special-operations-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res = "";
+
+        for(char& c: s){
+            if(c=='*'){
+                if(res.length()>0){
+                    res.pop_back();
+                }
+            }
+            else if(c=='#'){
+                res += res;
+            }
+            else if(c=='%'){
+                reverse(res.begin(),res.end());
+            }
+            else{
+                res += c;
+            }
+        }
+        return res;
+    }
+};
