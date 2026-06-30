b822b8ba07f92569c99afd644a19384c60c85195
sipher-01 <sourav2003singhkatoch@gmail.com>
2026-06-30 23:32:54 +0000

    Solve Number of Substrings Containing All Three Characters [cpp]

--- number-of-substrings-containing-all-three-characters.cpp
+++ number-of-substrings-containing-all-three-characters.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-            int n = s.size();
-    map<char,int>mp;
-    mp['a']=0;
-    mp['b']=0;
-    mp['c']=0;
-    int l=0, r=0;
-    int ans=0;
-    while(r<n){
-        mp[s[r]]++;
-        while(mp['a']>0 && mp['b']>0 && mp['c']>0){
-            ans+=n-r;
-            mp[s[l]]--;
-            l++;
+        int l =0;
+        unordered_map<char,int>mp;
+        int n = s.length();
+
+        int ans = 0;
+        for(int r=0; r<n; r++){
+            mp[s[r]]++;
+            while(mp.size()>=3){
+                ans += (n-r);
+                mp[s[l]]--;
+                if(mp[s[l]]==0){
+                    mp.erase(s[l]);
+                }
+                l++;
+            }
         }
-        r++;
-    }
-    return ans;
+
+        return ans;
     }
 };
