fb272e73d64b82edc7ab34eac1498f14e50f2875
sipher-01 <sourav2003singhkatoch@gmail.com>
2026-08-02 01:57:27 +0000

    Solve Predict the Winner [cpp]

--- predict-the-winner.cpp
+++ predict-the-winner.cpp
@@ -1,23 +1,27 @@
 class Solution {
+    int n;
 public:
-    int solve(int i, int j,int turn, vector<int>&nums, vector<vector<int>>&dp){
-        if(i>j)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int ans= 0;
+    bool solve(int l, int r, int cnt1, int cnt2, bool turn, vector<int>&nums){
+        if(l>r){
+            cout<<format("cnt1 : {} & cnt2 : {}",cnt1,cnt2)<<endl;
+            if(cnt1>=cnt2)return true;
+            return false;
+        }
+        bool res = false;
+        cout<<format("l : {} r : {} turn : {}",l,r,turn)<<endl;
         if(turn){
-            ans = max(nums[i]+solve(i+1,j,0,nums,dp),
-            nums[j]+solve(i,j-1,0,nums,dp));
+            res |= solve(l+1,r,cnt1+nums[l],cnt2,!turn,nums) ||
+             solve(l,r-1,cnt1+nums[r],cnt2,!turn,nums);
         }
         else{
-            ans = min(-nums[i]+solve(i+1,j,1,nums,dp),
-            -nums[j]+solve(i,j-1,1,nums,dp));
+            res |= solve(l+1,r,cnt1,cnt2+nums[l],!turn,nums) && 
+             solve(l,r-1,cnt1,cnt2+nums[r],!turn,nums);
         }
-        return dp[i][j] = ans;
+        return res;
     }
     bool predictTheWinner(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        if(n&1)return (solve(0,n-1,1,nums,dp)>=0);
+        n = nums.size();
+        if(n&1) return solve(0, n-1, 0, 0, true, nums);
         return true;
     }
 };
